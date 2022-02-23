d856a46ffa4ac061c69f18b7d4f043185dbb9bf1
Nathan <nathanparkengineering@gmail.com>
2022-02-23 13:56:57 -0800

    removed the bleh files and .vscode file

---
+++ new_york_spelling_bee_solver.py
@@ -0,0 +1,22 @@
+from english_words import english_words_lower_alpha_set
+
+def solver(letters, special):
+    answer = set()
+    for word in english_words_lower_alpha_set:
+        valid = False
+        if special in word:
+            valid = True
+
+        for c in word:
+            if c not in letters:
+                valid = False
+                break
+            
+        if valid: answer.add(word)
+
+    for word in sorted(answer, key=len):
+        if len(word) >= 4: print(word)
+
+solver(['n','l','b','u','e','i', 'f'], 'f')
+
+#test
